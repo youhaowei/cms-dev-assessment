f8003c62179e53d79621b01ddc6c4e63f40d6745
Amir R Muntasser <arkmuntasser@gmail.com>
2018-09-06 17:12:23 -0700

    updated assessment 3 to load in events and offers

--- solutions/3-via-api-filtering/main.js
+++ solutions/3-via-api-filtering/main.js
@@ -7,13 +7,34 @@ const app = new Vue({
     },
     methods: {
         getPlaceholderImage: function(e) {
-            e.target.src = '/assets/images/placeholder.jpg';
+            e.target.src = 'assets/images/placeholder.jpg';
+        },
+        standardizeImages: function(array, type) {
+            
         }
     },
     created: async function() {
-        const resPromise = await fetch('https://sv-reqres.now.sh/api/listings/?per_page=6');
-        const res = await resPromise.json();
+        const listingsPromise = await fetch('https://sv-reqres.now.sh/api/listings/?per_page=6');
+        const listings = await listingsPromise.json();
 
-        this.items = res.data;
+        listings.data.forEach(item => {
+            item.asset = item.primary_image_url || 'assets/images/placeholder.jpg';
+        });
+
+        const eventsPromise = await fetch('https://sv-reqres.now.sh/api/events/?per_page=6');
+        const events = await eventsPromise.json();
+
+        events.data.forEach(item => {
+            item.asset = item.media_raw ? item.media_raw[0].mediaurl : 'assets/images/placeholder.jpg';
+        });
+
+        const offersPromise = await fetch('https://sv-reqres.now.sh/api/offers/?per_page=6');
+        const offers = await offersPromise.json();
+
+        offers.data.forEach(item => {
+            item.asset = item.media ? item.media[0].mediaurl : 'assets/images/placeholder.jpg';
+        });
+
+        this.items = listings.data.concat(events.data).concat(offers.data);
     }
 });
