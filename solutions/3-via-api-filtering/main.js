4489d9a1c5719b38e2e44d12ed79977f9c9fb73d
Amir R Muntasser <arkmuntasser@gmail.com>
2018-09-07 14:13:59 -0700

    Completed 3rd level of assessment

--- solutions/3-via-api-filtering/main.js
+++ solutions/3-via-api-filtering/main.js
@@ -3,38 +3,44 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data: {
-        items: []
+        types: ['listings', 'events', 'offers'],
+        filter_raw: 'all',
+        listings: [],
+        events: [],
+        offers: []
+    },
+    computed: {
+        filter: function() {
+            if (this.filter_raw === 'all') { return this.types; }
+
+            return this.types.filter(type => type === this.filter_raw);
+        },
+        items: function() {
+            const items = [];
+
+            this.filter.forEach((type) => items.push(this[type]))
+            
+            return [].concat(...items);
+        },
     },
     methods: {
         getPlaceholderImage: function(e) {
             e.target.src = 'assets/images/placeholder.jpg';
         },
-        standardizeImages: function(array, type) {
-            
+        setFilter: function(e, type) {
+            document.querySelectorAll('.controls .btn').forEach(btn => btn.classList.remove('active'));
+            e.target.classList.add('active');
+
+            this.filter_raw = type;
+        },
+        getData: async function(type) {
+            const promise = await fetch(`https://sv-reqres.now.sh/api/${type}/?per_page=6`);
+            const res = await promise.json();
+
+            this[type] = res.data;
         }
     },
-    created: async function() {
-        const listingsPromise = await fetch('https://sv-reqres.now.sh/api/listings/?per_page=6');
-        const listings = await listingsPromise.json();
-
-        listings.data.forEach(item => {
-            item.asset = item.primary_image_url || 'assets/images/placeholder.jpg';
-        });
-
-        const eventsPromise = await fetch('https://sv-reqres.now.sh/api/events/?per_page=6');
-        const events = await eventsPromise.json();
-
-        events.data.forEach(item => {
-            item.asset = item.media_raw ? item.media_raw[0].mediaurl : 'assets/images/placeholder.jpg';
-        });
-
-        const offersPromise = await fetch('https://sv-reqres.now.sh/api/offers/?per_page=6');
-        const offers = await offersPromise.json();
-
-        offers.data.forEach(item => {
-            item.asset = item.media ? item.media[0].mediaurl : 'assets/images/placeholder.jpg';
-        });
-
-        this.items = listings.data.concat(events.data).concat(offers.data);
+    created: function() {
+        this.types.forEach(type => this.getData(type));
     }
 });
